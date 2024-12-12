7e2cd5e27a9165b0ea3d2ddc7813e7357bf179d3
Kanishka Mittal <kanishkamittal@outlook.in>
2024-12-12 11:59:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'Evencia',
+  description: 'Evencia is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
